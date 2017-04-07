4b7f7ca0494e03f1b6a0983ecdccb64e4ed11b66
aggarwalsomya <aggarwal.somya@gmail.com>
2017-04-07 15:16:10 -0700

    Rename components
    Rename components

--- uipro/src/main/java/com/vaadin/service/SettingReadingSessionAttributesUI.java
+++ uipro/src/main/java/com/vaadin/service/SettingReadingSessionAttributesUI.java
@@ -1,20 +1,14 @@
-package com.example.helloworld;
+package com.vaadin.service;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
